f5a1201ac8f37870b0cc0a90b9201e48ce302d2a
pLumbum82 <jobbsy@yandex.ru>
2023-11-08 16:34:54 +0500

    add s7-t3

---
+++ src/main/java/s7/Compare.java
@@ -0,0 +1,22 @@
+package s7;
+
+public class Compare<T> extends Number {
+    @Override
+    public int intValue() {
+        return 0;
+    }
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+}
